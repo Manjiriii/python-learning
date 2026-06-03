b10771d2cfe6cb390de568bf751445a84b95b5f1
Manjiri Chavan <manjiri8126@gmail.com>
2026-06-03 13:04:28 +0530

    Added grade calculator program using if-elif-else

---
+++ Basics/grade_calculator.py
@@ -0,0 +1,15 @@
+pct=int(input("Enter percentage: "))
+if pct>100 or pct<0:
+    print("Invalid percentage")
+elif pct>=90:
+    print("O grade")
+elif pct>=85:
+    print("A+ grade")
+elif pct>=75:
+    print("A grade")
+elif pct>=60:
+    print("B+ grade")
+elif pct>=50:
+    print("B grade")
+else:
+    print("Try again")
