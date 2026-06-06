948a75f1f59170714e0603d68139d3b4650fa2cc
Manjiri Chavan <manjiri8126@gmail.com>
2026-06-06 11:35:22 +0530

    Added odd and even numbers program using for loop

---
+++ Basics/odd_even_numbers_upto_100.py
@@ -0,0 +1,13 @@
+n = 100
+
+print("Odd & Even numbers up to 100:")
+
+print("Even numbers:")
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        print(i, end=", ")
+
+print("\nOdd numbers:")
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        print(i, end=", ")
