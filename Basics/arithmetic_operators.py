4b9608533a0f4234c8b4cf25916cdb9a3ec07d9c
Manjiri Chavan <manjiri8126@gmail.com>
2026-06-13 18:30:43 +0530

    Update arithmetic_operators.py

--- Basics/arithmetic_operators.py
+++ Basics/arithmetic_operators.py
@@ -8,3 +8,15 @@ print("Exponential (x**y): ",x**y)
 print("Division (x/y): ",x/y)
 print("Floor division (Integer division) (x//y): ",x//y)
 print("Modulus (x%y): ",x%y)
+
+# Example Output:
+# Enter x: 10
+# Enter y: 5
+# -------Arithmetic Operators-------
+# Addition (x+y): 15
+# Subtraction (x-y): 5
+# Multiplication (x*y): 50
+# Exponential (x**y): 100000
+# Division (x/y): 2.0
+# Floor division (Integer division) (x//y): 2
+# Modulus (x%y): 0
