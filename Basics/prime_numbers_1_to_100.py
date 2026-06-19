1cf72be65ea91fa5c7a2587530f0466f683e8188
Manjiri Chavan <manjiri8126@gmail.com>
2026-06-19 17:02:22 +0530

    Added program to print prime numbers from 1 to 100

--- Basics/prime_numbers_1_to_100.py
+++ Basics/prime_numbers_1_to_100.py
@@ -11,3 +11,10 @@ for n in range(1, 101):
 
     else:
         print(n, end=" ")
+        
+
+# Output:
+# Prime numbers from 1 to 100:
+# 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+
+
